56dafbf2d81191700c7312db51feced5c6671a8c
jyi2ya <jyi2ya@outlook.com>
2023-12-04 18:56:18 +0800

    test: add pdf target

--- generate-test.sh
+++ generate-test.sh
@@ -1,2 +1,3 @@
 perl convert.pl typora-theme-lapis/lapis.css > lapis.css 
 pandoc  --standalone --self-contained --toc  test/styles.md  -t html5  --css lapis.css -o output.html
+pandoc --standalone --self-contained --toc test/styles.md --pdf-engine wkhtmltopdf -t html5 --css lapis.css -o output.pdf
